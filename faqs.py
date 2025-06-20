37a090fc767d597fae6689c5e51eb2b4609de910
Santhosh-p653 <santhosh11042007@gmail.com>
2025-06-20 22:14:48 +0530

    Create faqs.py

---
+++ faqs.py
@@ -0,0 +1,5 @@
+faqs = [
+    {"question": "How do I reset my password?", "answer": "Click on 'Forgot password' and follow the instructions."},
+    {"question": "Where can I view my order history?", "answer": "Go to your profile and select 'Order History'."},
+    
+]
