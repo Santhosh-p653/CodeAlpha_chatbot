00b24ef6b676003c0ade8406bc76b0ac6d4901ea
Santhosh-p653 <santhosh11042007@gmail.com>
2025-06-28 17:05:22 +0530

    Delete chatbot.py

--- chatbot.py
+++
@@ -1,8 +0,0 @@
-from matching import get_best_match
-
-while True:
-    user_input = input("You: ")
-    if user_input.lower() in ("exit", "quit"):
-        break
-    answer = get_best_match(user_input)
-    print("Bot:", answer)
